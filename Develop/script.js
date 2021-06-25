7a46c1a50796fbca57274065f9a029d1d0ac3043
Dinimar Fish <tayla@tallisman.com.au>
2021-06-25 21:18:49 +1000

    removing while and if else statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
+var passcodeLength = "";
+
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { }
 
 //when the button is pressed, User should be asked how long they want the
 // Password to be.
+
+
+// passcodeLength = window.prompt("How many characters would you like the password to have?")
+
+
+// if (passcodeLength < 8 || passcodeLength > 128) {
+//   window.alert("Enter a number between 8 and 128");
+//   console.log("password is too short or too long");
+//   return;
+// } else {
+//   passcodeLength = window.prompt("How many characters would you like the password to have?")
+//   console.log("This we can work with");
+// }
+ 
 
   var passcodeLength = window.prompt("How long should the password be?", "8 <= x => 128")
   console.log(passcodeLength);
@@ -20,13 +36,16 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-}
 
 
 
 
 
-
+// while (passcodeLength < 8 || passcodeLength > 128) {
+//   window.alert("Enter a number between 8 and 128");
+//   window.prompt("How many characters would you like the password to have?")
+// }
+ 
 
 
 
